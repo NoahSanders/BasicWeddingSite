9b84ac3bf057eba802272fd16d925b42fb2b3496
Noah Sanders <noahcsan@gmail.com>
2017-09-05 23:12:33 -0500

    adding some RSVP functionality - still need to get AJAX call for search working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
-document.onload = pageSetup;
+$(document).ready(pageSetup);
+var RsvpApiUrl = 'http://localhost:3000/guests';
 
 function pageSetup() {
-    
+    //Set up add to calendar
+    if (window.addtocalendar) if(typeof window.addtocalendar.start == "function") return;
+    if (window.ifaddtocalendar == undefined) { 
+        window.ifaddtocalendar = 1;
+        var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+        s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
+        s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
+        var h = d[g]('body')[0];h.appendChild(s);
+    }
+
+    //set up handlers for RSVP buttons
+    $('#rsvp_submit').click(rsvpClick);
+    $('#name_search_submit').click(findGuest);
 }
 
+function rsvpClick() {
+    //do stuff here for rsvp
+
+    $('#rsvp_modal').modal('toggle');
+}
+
+function findGuest() {  
+    var lastName = $('#rsvp_last_name').val();
+    if (!lastName) {
+        //TODO: Error handling here
+        return;
+    }
+
+    $.ajax(RsvpApiUrl + '/search/' + lastName, {
+        type: 'GET',
+        dataType: 'jsonp',
+        jsonpCallback: jsonpCallback
+    });
+}
+
+function jsonpCallback(json) {
+    console.log(arguments);
+}
