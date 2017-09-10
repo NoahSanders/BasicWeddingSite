37d87fbe1bf87d0ca69a518babb8c55a0f4ecad2
Noah Sanders <noahcsan@gmail.com>
2017-09-10 00:19:41 -0500

    added more RSVP form functionality

--- js/script.js
+++ js/script.js
@@ -13,30 +13,103 @@ function pageSetup() {
     }
 
     //set up handlers for RSVP buttons
-    $('#rsvp_submit').click(rsvpClick);
+    $('#rsvp_submit').click(rsvpSubmit);
     $('#name_search_submit').click(findGuest);
+
+    $("#status_select").bootstrapToggle({
+        on: "I will attend",
+        off: "I will not attend",
+        width:"100%",
+        offstyle:"danger"
+    });
+
+    $('#status_select').change(function() {
+        let currentStatus = $(this).prop('checked');
+        if (currentStatus) {
+            $('#guest_number_input').removeClass('hidden');
+            let currentGuestCount = Number.parseInt($('#guest_count').val());
+            console.log(currentGuestCount);
+            if(currentGuestCount) {
+                $("#rsvp_submit").prop('disabled',false);
+            }
+            else {
+                $("#rsvp_submit").prop('disabled',true);
+            }
+        }
+        else {
+            $('#guest_number_input').addClass('hidden'); 
+            $("#rsvp_submit").prop('disabled',false);
+        }
+    });
+
+    $("#guest_count").change(function() {
+        $('#rsvp_submit').prop('disabled', false);
+
+        var currentVal = $('#guest_count').val();
+        if (currentVal = 1){
+            $('#minusOne').prop('disabled', true);
+        }
+        else {
+            $('#minusOne').prop('disabled', false);
+        }
+    });
+
+    $("#plusOne").click(function() {
+        var currentVal = Number.parseInt($('#guest_count').val());
+        if (!currentVal) {
+            $('#guest_count').val(1);
+        }
+        else {
+            $('#guest_count').val(currentVal+1);
+        }
+
+        $("#rsvp_submit").prop('disabled',false);
+    });
+
+    $("#minusOne").click(function() {
+        var currentVal = Number.parseInt($('#guest_count').val());
+        if (currentVal<=1) {
+            $('#guest_count').val(1);
+        }
+        else {
+            $('#guest_count').val(currentVal-1);
+        }
+
+        $("#rsvp_submit").prop('disabled',false);
+    });
 }
 
-function rsvpClick() {
+function rsvpSubmit() {
     //do stuff here for rsvp
 
     $('#rsvp_modal').modal('toggle');
 }
 
 function findGuest() {  
+    $('#status_select_section').addClass('hidden');
     var lastName = $('#rsvp_last_name').val();
-    if (!lastName) {
-        //TODO: Error handling here
-        return;
-    }
 
-    $.ajax(RsvpApiUrl + '/search/' + lastName, {
-        type: 'GET',
-        dataType: 'jsonp',
-        jsonpCallback: jsonpCallback
-    });
-}
+    $.get(RsvpApiUrl + '/?lastName=' + lastName)
+        .done(function(data) {
+            var choicesHTML = '';
+            if (data.guests.length) {
+                for (var i=0; i<data.guests.length; i++) {
+                    choicesHTML += '<a href="#" class="list-group-item list-group-item-action" id="' + data.guests[i] +'">' + data.guests[i].name + '</li>';
+                }
+            }
+            else {
+                choicesHTML = '<li class="list-group-item list-group-item-action disabled">No results found for selected last name.</li>';
+            }
 
-function jsonpCallback(json) {
-    console.log(arguments);
+            $('#full_name_select').html(choicesHTML);
+
+            $('a.list-group-item').click(function(e) {
+                e.preventDefault();
+                $(this).addClass('active').siblings().removeClass('active');
+                $('#status_select_section').removeClass('hidden');
+                $("#rsvp_submit").prop('disabled',false);
+            });
+
+            $('#guest_full_name_input').removeClass('hidden');
+        });
 }
